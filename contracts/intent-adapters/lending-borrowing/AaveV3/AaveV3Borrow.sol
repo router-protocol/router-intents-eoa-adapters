f9ce3f132e2620100fb4d2eb435c2a5d0e306421
Shivam Agrawal <shivam.agrawal7828@gmail.com>
2023-12-15 13:44:13 +0530

    added tests for AaveV3 Supply and Borrow adapters

---
+++ contracts/intent-adapters/lending-borrowing/AaveV3/AaveV3Borrow.sol
@@ -0,0 +1,135 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {AaveV3Helpers} from "./AaveV3Helpers.sol";
+import {RouterIntentAdapter, NitroMessageHandler, Errors} from "router-intents/contracts/RouterIntentAdapter.sol";
+import {IERC20, SafeERC20} from "../../../utils/SafeERC20.sol";
+
+/**
+ * @title AaveV3Borrow
+ * @author Shivam Agrawal
+ * @notice Borrowing funds on AaveV3.
+ */
+contract AaveV3Borrow is RouterIntentAdapter, AaveV3Helpers {
+    using SafeERC20 for IERC20;
+
+    address private immutable _self;
+
+    constructor(
+        address __native,
+        address __wnative,
+        address __assetForwarder,
+        address __dexspan,
+        address __defaultRefundAddress,
+        address __aaveV3Pool,
+        address __aaveV3WrappedTokenGateway,
+        uint16 __aaveV3ReferralCode
+    )
+        RouterIntentAdapter(
+            __native,
+            __wnative,
+            __assetForwarder,
+            __dexspan,
+            __defaultRefundAddress
+        )
+        AaveV3Helpers(
+            __aaveV3Pool,
+            __aaveV3WrappedTokenGateway,
+            __aaveV3ReferralCode
+        )
+    // solhint-disable-next-line no-empty-blocks
+    {
+        _self = address(this);
+    }
+
+    function name() public pure override returns (string memory) {
+        return "AaveV3Supply";
+    }
+
+    /**
+     * @inheritdoc RouterIntentAdapter
+     */
+    function execute(
+        address,
+        address,
+        bytes calldata data
+    ) external payable override returns (address[] memory tokens) {
+        (
+            uint256 amount,
+            uint256 rateMode,
+            address asset,
+            address onBehalfOf,
+            address recipient
+        ) = parseInputs(data);
+
+        bytes memory logData;
+
+        (tokens, logData) = _aaveV3Borrow(
+            amount,
+            rateMode,
+            asset,
+            onBehalfOf,
+            recipient
+        );
+
+        emit ExecutionEvent(name(), logData);
+        return tokens;
+    }
+
+    /**
+     * @inheritdoc NitroMessageHandler
+     */
+    function handleMessage(
+        address tokenSent,
+        uint256 amount,
+        bytes memory
+    ) external override onlyNitro nonReentrant {
+        withdrawTokens(tokenSent, defaultRefundAddress(), amount);
+        emit UnsupportedOperation(tokenSent, defaultRefundAddress(), amount);
+    }
+
+    //////////////////////////// ACTION LOGIC ////////////////////////////
+
+    /**
+     * @notice function to borrow funds from AaveV3.
+     * @param amount Amount of asset to be borrowed.
+     * @param rateMode Rate mode for borrowing. 1 for Stable Rate and 2 for Variable Rate
+     * @param asset Asset to be borrowed.
+     * @param onBehalfOf The user who will incur the borrow.
+     */
+    function _aaveV3Borrow(
+        uint256 amount,
+        uint256 rateMode,
+        address asset,
+        address onBehalfOf,
+        address recipient
+    ) private returns (address[] memory tokens, bytes memory logData) {
+        aaveV3Pool().borrow(
+            asset,
+            amount,
+            rateMode,
+            aaveV3ReferralCode(),
+            onBehalfOf
+        );
+
+        withdrawTokens(asset, recipient, amount);
+
+        tokens = new address[](1);
+        tokens[0] = asset;
+
+        logData = abi.encode(amount, rateMode, asset, onBehalfOf);
+    }
+
+    /**
+     * @dev function to parse input data.
+     * @param data input data.
+     */
+    function parseInputs(
+        bytes memory data
+    ) public pure returns (uint256, uint256, address, address, address) {
+        return abi.decode(data, (uint256, uint256, address, address, address));
+    }
+
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+}
