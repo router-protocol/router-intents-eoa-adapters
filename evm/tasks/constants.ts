975bb6438ee14a292087fcbf4babe06a91c5a6bb
Yashika Goyal <yashika@routerprotocol.com>
2024-06-17 01:03:19 +0530

    feat: added lynexGamma

--- evm/tasks/constants.ts
+++ evm/tasks/constants.ts
@@ -194,6 +194,8 @@ export const DEPLOY_ASSET_BRIDGE_ADAPTER = "DEPLOY_ASSET_BRIDGE_ADAPTER";
 export const VERIFY_ASSET_BRIDGE_ADAPTER = "VERIFY_ASSET_BRIDGE_ADAPTER";
 export const DEPLOY_THIRDFY_SWAP_ADAPTER = "DEPLOY_THIRDFY_SWAP_ADAPTER";
 export const VERIFY_THIRDFY_SWAP_ADAPTER = "VERIFY_THIRDFY_SWAP_ADAPTER";
+export const DEPLOY_LYNEX_GAMMA_ADAPTER = "DEPLOY_LYNEX_GAMMA_ADAPTER";
+export const VERIFY_LYNEX_GAMMA_ADAPTER = "VERIFY_LYNEX_GAMMA_ADAPTER";
 
 export const ASSET_FORWARDER: {
   [network: string]: { [chainId: string]: string };
@@ -407,4 +409,5 @@ export const CONTRACT_NAME: { [key: string]: string } = {
   ThirdFyMint: "ThirdFyMint",
   AssetBridgeAdapter: "AssetBridgeAdapter",
   ThirdFySwap: "ThirdFySwap",
+  LynexGamma: "LynexGamma",
 };
--- evm/tasks/deploy/lynex/constants.ts
+++ evm/tasks/deploy/lynex/constants.ts
@@ -1,3 +1,7 @@
 export const LYNEX_ROUTER: { [chainId: string]: string } = {
   "59144": "0x610D2f07b7EdC67565160F587F37636194C34E74",
 };
+
+export const LYNEX_GAMMA: { [chainId: string]: string } = {
+  "59144": "0xFc13Ebe7FEB9595D70195E9168aA7F3acE153621",
+};
