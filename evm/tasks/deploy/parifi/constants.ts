7a38227671edb217ff2161949beb82ea3efe061d
Yashika Goyal <yashika@routerprotocol.com>
2024-08-14 22:24:47 +0530

    feat: added parifi vault deposit adapter contracts

--- evm/tasks/deploy/parifi/constants.ts
+++ evm/tasks/deploy/parifi/constants.ts
@@ -5,3 +5,16 @@ export const PARIFI_ORDER_MANAGER: { [chainId: string]: string } = {
 export const PARIFI_DATA_FABRIC: { [chainId: string]: string } = {
   "42161": "0x4df30D2B4D595Ca252AEC77e4f8623E545b5021e",
 };
+
+export const USDC: { [chainId: string]: string } = {
+  "42161": "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+};
+
+export const PF_USDC: { [chainId: string]: string } = {
+  "42161": "0xbf11aC635fE97430b5333018778d0D746f67C989",
+};
+
+export const PF_WETH: { [chainId: string]: string } = {
+  "42161": "0xD979e6D8A05Ef11B185d2DF3d76175EEcae522Af",
+};
+
