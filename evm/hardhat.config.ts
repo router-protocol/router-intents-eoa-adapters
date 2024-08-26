672e2f9d9b9ac026f1c9255f248cfcc897e4de67
Yashika Goyal <yashika@routerprotocol.com>
2024-08-26 12:51:16 +0530

    added deployments for parifi vault deposit

--- evm/hardhat.config.ts
+++ evm/hardhat.config.ts
@@ -187,7 +187,7 @@ const config: HardhatUserConfig = {
       chainId: 43113,
     },
     arbitrum: {
-      url: "https://public.stackup.sh/api/v1/node/arbitrum-one",
+      url: "https://api.stateless.solutions/arbitrum-one/v1/demo",
       accounts:
         process.env.PRIVATE_KEY !== undefined
           ? [process.env.PRIVATE_KEY]
